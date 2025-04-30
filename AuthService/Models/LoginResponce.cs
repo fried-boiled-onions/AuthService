9229bf1789566579f7b840c598cbdab5a5f83636
borutiel <borutiel@gmail.com>
2025-04-30 13:29:47 +0300

    Add files via upload

---
+++ AuthService/Models/LoginResponce.cs
@@ -0,0 +1,8 @@
+namespace AuthService.Models
+{
+    public class LoginResponse
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
---
+++ AuthService/Program.cs
@@ -0,0 +1,20 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient();
+builder.Services.AddHttpClient("DebugClient")
+    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = (msg, cert, chain, errors) => true
+    });
+var app = builder.Build();
+
+app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "AuthService API V1");
+});
+app.Run();
